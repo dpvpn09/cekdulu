037da67ddf48fc0717c5cc9f874f1548ab17658c
dpvpn09 <92319385+dpvpn09@users.noreply.github.com>
2021-10-12 15:39:45 +0700

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -20,19 +20,19 @@ mkdir /var/lib/premium-script;
 echo "IP=" >> /var/lib/premium-script/ipvps.conf
 wget https://raw.githubusercontent.com/dpvpn09/cekdulu/main/cf.sh && chmod +x cf.sh && ./cf.sh
 #install ssh ovpn
-wget https://raw.githubusercontent.com/dpvpn09/cekdulu/main/ssh-vpn.sh && chmod +x ssh-vpn.sh && screen -S ssh-vpn ./ssh-vpn.sh
-wget https://raw.githubusercontent.com/dpvpn09/cekdulu/main/sstp.sh && chmod +x sstp.sh && screen -S sstp ./sstp.sh
+wget https://raw.githubusercontent.com/dpvpn09/cekdulu/main/ssh-vpn.sh && chmod +x ssh-vpn.sh &&  ./ssh-vpn.sh
+wget https://raw.githubusercontent.com/dpvpn09/cekdulu/main/sstp.sh && chmod +x sstp.sh &&  ./sstp.sh
 #install ssr
-wget https://raw.githubusercontent.com/dpvpn09/cekdulu/main/ssr.sh && chmod +x ssr.sh && screen -S ssr ./ssr.sh
-wget https://raw.githubusercontent.com/dpvpn09/cekdulu/main/sodosok.sh && chmod +x sodosok.sh && screen -S ss ./sodosok.sh
+wget https://raw.githubusercontent.com/dpvpn09/cekdulu/main/ssr.sh && chmod +x ssr.sh &&  ./ssr.sh
+wget https://raw.githubusercontent.com/dpvpn09/cekdulu/main/sodosok.sh && chmod +x sodosok.sh &&  ./sodosok.sh
 #installwg
-wget https://raw.githubusercontent.com/dpvpn09/cekdulu/main/wg.sh && chmod +x wg.sh && screen -S wg ./wg.sh
+wget https://raw.githubusercontent.com/dpvpn09/cekdulu/main/wg.sh && chmod +x wg.sh && ./wg.sh
 #install v2ray
-wget https://raw.githubusercontent.com/dpvpn09/cekdulu/main/ins-vt.sh && chmod +x ins-vt.sh && screen -S v2ray ./ins-vt.sh
+wget https://raw.githubusercontent.com/dpvpn09/cekdulu/main/ins-vt.sh && chmod +x ins-vt.sh &&  ./ins-vt.sh
 #install L2TP
-wget https://raw.githubusercontent.com/dpvpn09/cekdulu/main/ipsec.sh && chmod +x ipsec.sh && screen -S ipsec ./ipsec.sh
+wget https://raw.githubusercontent.com/dpvpn09/cekdulu/main/ipsec.sh && chmod +x ipsec.sh &&  ./ipsec.sh
 wget https://raw.githubusercontent.com/dpvpn09/cekdulu/main/set-br.sh && chmod +x set-br.sh && ./set-br.sh
-wget https://queenssh.tk/wdkusc/websocket.sh && chmod +x websocket.sh && screen -S websocket ./websocket.sh
+wget https://queenssh.tk/wdkusc/websocket.sh && chmod +x websocket.sh && ./websocket.sh
 rm -f /root/ssh-vpn.sh
 rm -f /root/weleh.sh
 rm -f /root/sstp.sh
@@ -105,7 +105,7 @@ echo "   - Auto Delete Expired Account" | tee -a log-install.txt
 echo "   - Full Orders For Various Services" | tee -a log-install.txt
 echo "   - White Label" | tee -a log-install.txt
 echo "   - Installation Log --> /root/log-install.txt"  | tee -a log-install.txt
-echo "------------------Script Created By TuanYz-----------------" | tee -a log-install.txt
+echo "------------------Script Created By DP VPN-----------------" | tee -a log-install.txt
 
 echo " Reboot 15 Sec"
 sleep 15
