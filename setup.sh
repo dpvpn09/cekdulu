56f2866c0c654017f910942024d5702a983c36a7
dpvpn09 <92319385+dpvpn09@users.noreply.github.com>
2021-10-12 23:53:06 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -86,8 +86,8 @@ echo "   >>> Service & Port"  | tee -a log-install.txt
 echo "   - OpenSSH                 : 22"  | tee -a log-install.txt
 echo "   - OpenVPN                 : TCP 1194, UDP 2200, SSL 442"  | tee -a log-install.txt
 echo "   - Stunnel4                : 777"  | tee -a log-install.txt
-echo "   - Dropbear                : 445, 143"  | tee -a log-install.txt
-echo "   - Websocket Http's        : 443, 2082" | tee -a log-install.txt
+echo "   - Dropbear                : 445, 990"  | tee -a log-install.txt
+echo "   - Websocket Http's        : 443, 8880" | tee -a log-install.txt
 echo "   - Squid Proxy             : 3128, 8080(limit to IP Server)"  | tee -a log-install.txt
 echo "   - Badvpn                  : 7100, 7200, 7300"  | tee -a log-install.txt
 echo "   - Nginx                   : 81"  | tee -a log-install.txt
@@ -101,7 +101,7 @@ echo "   - SS-OBFS HTTP            : 3443-3543"  | tee -a log-install.txt
 echo "   - V2RAY Vmess TLS         : 8443"  | tee -a log-install.txt
 echo "   - V2RAY Vmess None TLS    : 80"  | tee -a log-install.txt
 echo "   - V2RAY Vless TLS         : 2083"  | tee -a log-install.txt
-echo "   - V2RAY Vless None TLS    : 8880"  | tee -a log-install.txt
+echo "   - V2RAY Vless None TLS    : 2082"  | tee -a log-install.txt
 echo "   - Trojan                  : 2087"  | tee -a log-install.txt
 echo "   >>> Server Information & Other Features"  | tee -a log-install.txt
 echo "   - Timezone                : Asia/Jakarta (GMT +7)"  | tee -a log-install.txt
